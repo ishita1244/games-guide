33d60ecbb09d266ef4dba896ca41cc8196592514
ishita <ishitasingla8@gmail.com>
2021-11-23 16:23:28 +0530

    commit 2

---
+++ scripts/auth.js
@@ -0,0 +1,78 @@
+
+
+//listen for auth status changes
+auth.onAuthStateChanged(user => {
+    // console.log(user);
+    if (user) {
+        //getting data
+        db.collection('guides').onSnapshot((snapshot) => {
+            setupGuides(snapshot.docs);
+            setUpUI(user);
+        })
+    }
+    else {
+        setUpUI();
+        setupGuides([]);//we pass empty array because we actually dont want any output
+    }
+});//every time a user gets signed/login.logout this fun gets fired
+
+
+//create guide
+const createForm=document.querySelector('#create-form');
+createForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    db.collection('guides').add({
+        title:createForm['title'].value,
+        content:createForm['content'].value
+    }).then(()=>{
+        //clear the form and close the modal
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch((err)=>{
+        console.log(err.message);
+    })
+})
+
+
+//signup form
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    console.log(email, password);
+
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    })//it will take some time to complete so called promise function
+});
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log("user signed out");
+    })
+})
+
+//logging user in
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        //close the login and reset the form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    })
+
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,42 @@
+const guideList=document.querySelector('.guides');
+const loggedOutLinks=document.querySelectorAll('.logged-out');
+const loggedInLinks=document.querySelectorAll('.logged-in');
+const setUpUI=(user)=>{
+  if(user){
+    loggedInLinks.forEach(item=>item.style.display='block');
+    loggedOutLinks.forEach(item=>item.style.display='none');
+  }
+  else{
+    loggedInLinks.forEach(item=>item.style.display='none');
+    loggedOutLinks.forEach(item=>item.style.display='block');
+  }
+}
+
+//setup guides
+const setupGuides=(data)=>{
+
+  if(data.length){
+  let html='';
+  data.forEach(doc=>{
+    const guide=doc.data();
+    const li=`
+    <li>
+    <div class="collapsible-header grey lighten-4">${guide.title}</div>
+    <div class="collapsible-body white">${guide.content}</div>
+    </li>
+    `;
+    html+=li;
+    guideList.innerHTML=html;
+
+  })
+}
+else{
+  guideList.innerHTML='<h5 class="cenre-align">Login to view guides</h5>'
+
+}
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
     var modals = document.querySelectorAll('.modal');
